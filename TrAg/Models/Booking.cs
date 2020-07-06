94ed7968daff526ad8ee1e875c1d74e27b6b643e
nrlcpp <nrlcpp@gmail.com>
2020-07-06 22:21:42 +0300

    Add project files.

---
+++ TrAg/Models/Booking.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrAg.Models
+{
+    public enum Type
+    {
+        circuit,
+        stay,
+        accommodation,
+        others
+    }
+    public enum Currency
+    {
+        EUR,
+        RON,
+        USD
+    }
+    public class Booking
+    {
+        public long Id { get; set; }
+
+        //   public string Description { get; set; }
+
+        public long Sum { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime AddedOn { get; set; }
+
+        public Currency Currency { get; set; }
+
+        public Type Type { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public bool Documents { get; set; }
+
+       // public User AddedBy { get; set; }
+
+        public List<Remark> Remarks { get; set; }
+    }
+}
---
+++ TrAg/Models/Remark.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TrAg.Models
+{
+    public enum Importance
+    {
+        hight,
+        medium,
+        lower
+    }
+    public class Remark
+    {
+        public long Id { get; set; }
+        public string Agent { get; set; }
+        public string Content { get; set; }
+        public Importance Importance { get; set; }
+        public DateTime AddedOn { get; set; }
+        public long ReservationId { get; set; }
+        //public User AddedBy { get; set; }
+
+        [JsonIgnore]
+        public Booking Booking { get; set; }
+    }
+}
